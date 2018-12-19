73ad2f99ccfb38c61359e20ae03998d14b6fc97e
kstrubel <kstrubel@purdue.edu>
2018-12-19 16:05:07 -0500

    url update

--- travel/urls.py
+++ travel/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:country_code>/', views.country, name='country'),
+    path('country/<str:country_code>/', views.country, name='country'),
     path('', views.index, name='index'),
 ]
